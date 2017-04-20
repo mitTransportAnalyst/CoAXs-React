bf50250780a73283630bf407d1c3004cf4d01ce3
Xin Zheng <zhengxin@mit.edu>
2017-04-20 00:20:47 -0400

    heroku test

--- server.js
+++ server.js
@@ -20,17 +20,17 @@ const port = process.env.PORT;
 const public_path = express.static(path.join(__dirname + '/public'));
 const index_path = express.static(path.join(__dirname + '/public/index.html')) ;
 
-app.use(public_path);
-app.get('/hello', function (request, response) {
+// app.use(public_path);
+app.get('/', function (request, response) {
   response.send('GET request to the homepage')
 });
-app.get('/', function (request, response) {
-  response.sendFile(index_path, function (error) {
-    if (error) {
-      console.log(error);
-    }
-  });
-});
+// app.get('/', function (request, response) {
+//   response.sendFile(index_path, function (error) {
+//     if (error) {
+//       console.log(error);
+//     }
+//   });
+// });
 
 app.listen(port, function() {
   console.log("App is running on port " + port);
