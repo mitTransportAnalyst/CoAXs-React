c26285d142f505333de0f0c573e6faa160330932
Xin Zheng <zhengxin@mit.edu>
2017-04-20 00:28:02 -0400

    heroku

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const express = require('express');
 
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 const public_path = express.static(path.join(__dirname + '/public'));
 const index_path = express.static(path.join(__dirname + '/public/index.html')) ;
 
